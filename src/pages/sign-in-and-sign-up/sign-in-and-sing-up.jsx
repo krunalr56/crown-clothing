866ec0ba0e4a8b790a1876da93154c39b7721fb9
bob <44295504+krunalr56@users.noreply.github.com>
2020-12-23 20:31:18 +0530

    Implemented firebase and storing users in DB

--- src/pages/sign-in-and-sign-up/sign-in-and-sing-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sing-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../sign-in-and-sign-up/sign-in-and-sing-up.scss'
 
-import SignIn from '../../components/sign-in/sign-in.component'
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () =>(
     <div className='sign-in-and-sing-up'>
         <SignIn/> 
+        <SignUp/>
     </div>
 )
 export default SignInAndSignUpPage;
